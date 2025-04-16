83d0cc20f89002cd4e6fcf756ee9614fd0bfe0dc
jignesh@1001 <jigneshgurav12@gmail.com>
2025-04-17 00:39:38 +0530

    updated constant js to root url

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,2 +1,2 @@
 //  export const USER_API_END_POINT =  "http://localhost:8000/api/v1/user";
- export const USER_API_END_POINT =  "https://jobsy-y5gz.onrender.com/api/v1/user";
+ export const USER_API_END_POINT =  "https://jobsy-y5gz.onrender.com";
