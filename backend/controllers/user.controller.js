fdfd9f207bb45e955840724cab9199c81baa533f
jignesh@1001 <jigneshgurav12@gmail.com>
2025-04-17 01:37:14 +0530

    added keys to make comaptability with hosted frontend

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -92,7 +92,9 @@ export const login = async (req, res) => {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         // httpsOnly:true,
-        sameSite: "strict"
+        // sameSite: "strict"
+        sameSite: "none",
+        secure: true
       })
       .json({
         // message: `Welcome back ${user.fullname}`,
