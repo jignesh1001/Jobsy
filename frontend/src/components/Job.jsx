750bed8343697020b25b9f085f3c1630a2e97d3b
jignesh@1001 <jigneshgurav12@gmail.com>
2025-04-24 14:49:53 +0530

    added job card component

--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -1,9 +1,47 @@
-
-
+import { Bookmark } from "lucide-react";
+import { Button } from "./ui/button";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import {Badge} from './ui/badge';
 const Job = () => {
   return (
-    <div>Job</div>
-  )
-}
+    <div className="p-5 rounded-md shadow-xl bg-white border border-gray-100">
+      <div className="flex items-center justify-between">
+        <p className='text-sm text-gray-500'>2 days ago</p>
+        <Button variant="outline" className="rounded-full" size="icon">
+          <Bookmark />
+        </Button>
+      </div>
 
-export default Job
+      <div className="flex items-center gap-2 my-2">
+        <Button className="p-6" variant="outline" size="icon">
+          <Avatar>
+            <AvatarImage src="https://www.shutterstock.com/image-vector/air-conditioning-company-logo-ac-600nw-2509412195.jpg" />
+          </Avatar>
+        </Button>
+        <div>
+          <h1 className='font-medium text-lg'>Company Name</h1>
+          <p className='text-sm text-gray-500'>India</p>
+        </div>
+      </div>
+      <div>
+        <h1 className="font-bold text-lg my-2">Title</h1>
+        <p className="text-sm text-grey-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+          perspiciatis hic natus exercitationem provident recusandae porro,
+          accusantium delectus obcaecati veniam.
+        </p>
+      </div>
+      <div className="flex items-center gap-2 mt-4">
+            <Badge className={'text-blue-700 font-bold'} variant="ghost">12 Positions</Badge>
+            <Badge className={'text-[#F83002] font-bold'} variant="ghost">Part Time</Badge>
+            <Badge className={'text-[#7209b7] font-bold'} variant="ghost">24 LPA</Badge>
+        </div>
+      <div className='flex items-center gap-4 mt-4'>
+        <Button variant='outline'>Details</Button>
+        <Button className='bg-[#7209b7]'>Save For Later</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Job;
