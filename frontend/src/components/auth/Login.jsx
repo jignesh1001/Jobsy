73c080be63eaa33788f0cb01f2b774bc4605be4c
jignesh@1001 <jigneshgurav12@gmail.com>
2025-06-19 12:18:58 +0530

    tested toast successfully

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
       if (res.data.success) {
         dispatch(setUser(res.data.user))
         navigate("/");
-        // toast.success(res.data.message);
+        toast.success(res.data.message);
       }
     } catch (error) {
       console.log(error);
