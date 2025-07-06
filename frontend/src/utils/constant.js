5feee6ad49e587fc22792618c598cb8d83ec2fda
jignesh@1001 <jigneshgurav12@gmail.com>
2025-07-06 22:25:21 +0530

    chnages in constant js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,20 @@
- export const USER_API_END_POINT =  "http://localhost:8000/api/v1/user || https://jobsy-y5gz.onrender.com/api/v1/user";
- export const JOB_API_END_POINT =  "http://localhost:8000/api/v1/job || https://jobsy-y5gz.onrender.com/api/v1/job";
- export const APPLICATION_API_END_POINT =  "http://localhost:8000/api/v1/application || https://jobsy-y5gz.onrender.com/api/v1/application";
-//  export const USER_API_END_POINT =  "https://jobsy-y5gz.onrender.com/api/v1/user";
+//  export const USER_API_END_POINT =  "http://localhost:8000/api/v1/user || https://jobsy-y5gz.onrender.com/api/v1/user";
+//  export const JOB_API_END_POINT =  "http://localhost:8000/api/v1/job || https://jobsy-y5gz.onrender.com/api/v1/job";
+//  export const APPLICATION_API_END_POINT =  "http://localhost:8000/api/v1/application || https://jobsy-y5gz.onrender.com/api/v1/application";
+// //  export const USER_API_END_POINT =  "https://jobsy-y5gz.onrender.com/api/v1/user";
+
+
+
+const isLocalhost = window?.location?.hostname === "localhost";
+
+export const USER_API_END_POINT = isLocalhost
+  ? "http://localhost:8000/api/v1/user"
+  : "https://jobsy-y5gz.onrender.com/api/v1/user";
+
+export const JOB_API_END_POINT = isLocalhost
+  ? "http://localhost:8000/api/v1/job"
+  : "https://jobsy-y5gz.onrender.com/api/v1/job";
+
+export const APPLICATION_API_END_POINT = isLocalhost
+  ? "http://localhost:8000/api/v1/application"
+  : "https://jobsy-y5gz.onrender.com/api/v1/application";
