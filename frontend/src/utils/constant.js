b37606170297658e9aa690c77e4da711d25ca0ed
jignesh@1001 <jigneshgurav12@gmail.com>
2025-07-09 22:06:34 +0530

    updated constantjs

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,30 +1,30 @@
-//  export const USER_API_END_POINT =  "http://localhost:8000/api/v1/user || https://jobsy-y5gz.onrender.com/api/v1/user";
-//  export const JOB_API_END_POINT =  "http://localhost:8000/api/v1/job || https://jobsy-y5gz.onrender.com/api/v1/job";
-//  export const APPLICATION_API_END_POINT =  "http://localhost:8000/api/v1/application || https://jobsy-y5gz.onrender.com/api/v1/application";
+ export const USER_API_END_POINT =  "http://localhost:8000/api/v1/user || https://jobsy-y5gz.onrender.com/api/v1/user";
+ export const JOB_API_END_POINT =  "http://localhost:8000/api/v1/job || https://jobsy-y5gz.onrender.com/api/v1/job";
+ export const APPLICATION_API_END_POINT =  "http://localhost:8000/api/v1/application || https://jobsy-y5gz.onrender.com/api/v1/application";
 // //  export const USER_API_END_POINT =  "https://jobsy-y5gz.onrender.com/api/v1/user";
 
 
 
 
-const hostname = window?.location?.hostname;
-const isLocalhost = hostname === "localhost" || hostname === "127.0.0.1" || hostname === "::1";
+// const hostname = window?.location?.hostname;
+// const isLocalhost = hostname === "localhost" || hostname === "127.0.0.1" || hostname === "::1";
 
 
-export const USER_API_END_POINT = isLocalhost
-  ? "http://localhost:8000/api/v1/user"
-  : "https://jobsy-y5gz.onrender.com/api/v1/user";
+// export const USER_API_END_POINT = isLocalhost
+//   ? "http://localhost:8000/api/v1/user"
+//   : "https://jobsy-y5gz.onrender.com/api/v1/user";
 
-export const JOB_API_END_POINT = isLocalhost
-  ? "http://localhost:8000/api/v1/job"
-  : "https://jobsy-y5gz.onrender.com/api/v1/job";
+// export const JOB_API_END_POINT = isLocalhost
+//   ? "http://localhost:8000/api/v1/job"
+//   : "https://jobsy-y5gz.onrender.com/api/v1/job";
 
-export const APPLICATION_API_END_POINT = isLocalhost
-  ? "http://localhost:8000/api/v1/application"
-  : "https://jobsy-y5gz.onrender.com/api/v1/application";
+// export const APPLICATION_API_END_POINT = isLocalhost
+//   ? "http://localhost:8000/api/v1/application"
+//   : "https://jobsy-y5gz.onrender.com/api/v1/application";
 
-export const COMPANY_API_END_POINT = isLocalhost
-  ? "http://localhost:8000/api/v1/company"
-  : "https://jobsy-y5gz.onrender.com/api/v1/company";
+// export const COMPANY_API_END_POINT = isLocalhost
+//   ? "http://localhost:8000/api/v1/company"
+//   : "https://jobsy-y5gz.onrender.com/api/v1/company";
 
 // export const USER_API_END_POINT = "http://blog-env.eba-xmdhahad.ap-south-1.elasticbeanstalk.com/api/v1/user";
 // export const JOB_API_END_POINT = "http://blog-env.eba-xmdhahad.ap-south-1.elasticbeanstalk.com/api/v1/job";
